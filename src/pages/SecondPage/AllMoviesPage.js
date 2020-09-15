df419a85ac940baf463cac3c0e08a54c16db3306
renata2014 <x.rena_x@yahoo.com>
2020-09-15 18:14:42 +0300

    added Add Movie Button

--- src/pages/SecondPage/AllMoviesPage.js
+++ src/pages/SecondPage/AllMoviesPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FooterComponent } from '../../components/footer/FooterInfo';
 import { Header } from '../../components/header/Header';
+import { AddMovie } from '../SecondPage/AllMoviesPageContent';
+
 
 export function AllMoviesPage () {
     return(
+        <div>
         <Header />
-
+        <FooterComponent />
+        <AddMovie />
+        </div>
     )
 }
 

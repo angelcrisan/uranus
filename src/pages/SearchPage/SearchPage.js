56d5be68e7d7e69802ac05902ea2e1e7e330e6cc
renata2014 <x.rena_x@yahoo.com>
2020-09-19 21:19:31 +0300

    adding stile to search page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -3,21 +3,26 @@ import './SearchPage.css';
 export class SearchPage extends React.Component{
     render(){
         return(
+            <div>
+            <h3>Search By:</h3> 
             <div class="search">
-                <h3>Search By:</h3> <br></br>
-               <p>Title:</p> <input type="text"></input> <br></br>
-               <p>Year:</p> <input type="text"></input> <br></br>
-               <p>Runtime:</p> <input type="text"></input> <br></br>
-               <p>Genre:</p> <input type="text"></input> <br></br>
-               <p>Language:</p> <input type="text"></input> <br></br>
-               <p>Country:</p> <input type="text"></input> <br></br>
-               <p>Poster:</p> <input type="text"></input> <br></br>
-               <p>Rating:</p> <input type="text"></input> <br></br>
-               <p>Votes:</p> <input type="text"></input> <br></br>
-               <p>ID:</p> <input type="text"></input> <br></br>
-               <p>Type:</p> <input type="text"></input> <br></br>
+              <div class="first-rows"> <p>Title:</p> <input type="text"></input> 
+               <p>Year:</p> <input type="text"></input>
+               <p>Genre:</p> <input type="text" ></input>
+               <p>Language:</p> <input type="text"></input> 
+               <p>Country:</p> <input type="text"></input> 
+               </div>
+               <div className="second-rows">
+               <p>Poster:</p> <input type="text"></input> 
+               <p>Rating:</p> <input type="text"></input> 
+               <p>Votes:</p> <input type="text"></input> 
+               <p>ID:</p> <input type="text"></input>
+               <p>Type:</p> <input type="text"></input> 
+               </div>
                
-               <button id="search-button">Search</button>     
+               <button className="search-button">Search</button> 
+               
+            </div>
             </div>
            
         )

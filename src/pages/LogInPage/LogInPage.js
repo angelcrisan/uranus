67e0016d77a8b5ca16b50e12a05334b34190e9c7
renata2014 <x.rena_x@yahoo.com>
2020-09-19 11:30:41 +0300

    log-in-funcionality

--- src/pages/LogInPage/LogInPage.js
+++ src/pages/LogInPage/LogInPage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { FooterComponent } from '../../components/footer/FooterInfo';
-// import { Header } from '../../components/header/Header';
 import './LogInPage.css';
 import { LogIn } from './LogInPageContent';
 
--- src/pages/LogInPage/LogInPageContent.js
+++ src/pages/LogInPage/LogInPageContent.js
@@ -1,17 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,Redirect } from 'react-router-dom';
 import './LogInPageContent.css';
 
 
 export class LogIn extends React.Component{
+    state = {
+        username: '',
+        password:''
+      };
+  
+    
+    handleChangeUsername = event => this.setState({username: event.target.value}) 
+    
+    handleChangePassword = event => this.setState({password: event.target.value})
+
+    handleLogIn = ()=> {
+      fetch("https://movies-app-siit.herokuapp.com/auth/login", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+         if(this.state.username === '' || this.state.password === ''){
+            alert("Please provide values for Username or Password")
+            return
+            }if (json.message === "Wrong password") {
+            alert("Incorrect credentials!!")
+            return
+          } if (json.message === "User not found") {
+            alert("User not found! Not a menber yet?")
+            return
+          } 
+        localStorage.setItem("accessToken", json.accessToken);
+        return <Redirect to="/" />                
+        });
+    }
+
     render(){
+       
         return(
             <div className="page-content">
                 <h3 className="title">Log in</h3> 
                 <p>Username:</p>
-                <input></input>
+                <input 
+                    value={this.state.value} 
+                    onBlur={this.handleChangeUsername}>
+                </input>
                 <p>Password:</p>
-                <input className="last-input"></input>
+                <input 
+                    className="last-input" 
+                    type="password" 
+                    value={this.state.value} 
+                    onBlur={this.handleChangePassword}>
+                </input>
                 <br></br>
             <div className="linkToRegister">
                 <Link to="/Register-Page-Content">
@@ -19,7 +68,7 @@ export class LogIn extends React.Component{
                 </Link>
             </div>
             <br></br>
-                <button className="log-in-button">Log In</button>
+                <button className="log-in-button" onClick={this.handleLogIn} >Log In</button>
             </div>
         )
     }
